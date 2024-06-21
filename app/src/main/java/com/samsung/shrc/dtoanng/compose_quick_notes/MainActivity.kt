8e3d28cac200b5b025829034a825a7ea641ac997
Nguyen Duy Toan <duytoan.ng1@gmail.com>
2024-06-21 15:47:13 +0700

    init quick notes project

---
+++ app/src/main/java/com/samsung/shrc/dtoanng/compose_quick_notes/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.samsung.shrc.dtoanng.compose_quick_notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.samsung.shrc.dtoanng.compose_quick_notes.ui.theme.ComposequicknotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposequicknotesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Duy Toan",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposequicknotesTheme {
+        Greeting("Android")
+    }
+}
